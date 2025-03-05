2b7b057bc385dc47f2cd661cc5d0a231a8ad542f
Sujan <Sujan@tonic.com.au>
2025-03-05 12:37:49 +1100

    [feat] FEAT-000: Change to table view

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,14 @@ import { useEffect, useState } from "react"
 import { getAllPosts } from "../api/posts";
 import { Post } from "../types/Posts";
 import PostComponent from "../components/posts/Post";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table"
 
 
 export default function Dashboard(){
@@ -20,8 +28,19 @@ export default function Dashboard(){
 
 
   return <div className="container mx-auto flex flex-wrap gap-y-3">
-    {posts.map((post)=>(
+    <Table>
+    <TableCaption>List of Posts</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead className="w-[100px]">ID</TableHead>
+        <TableHead>Title</TableHead>
+        <TableHead>Content</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody> {posts.map((post)=>(
       <PostComponent {...post} />
-    ))}
+    ))}</TableBody>
+    </Table>
+   
   </div>
 }
